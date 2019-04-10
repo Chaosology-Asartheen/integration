86331dd250995757be1fb90f9a7d4221af8e9779
christinaou <christinaou@cmu.edu>
2019-04-10 12:09:57 -0400

    Adjusted for real time

--- cv/hough_circles.py
+++ cv/hough_circles.py
@@ -2,12 +2,14 @@
 import numpy as np
 import argparse
 import cv2
+from datetime import datetime
 
 DP = 1.2
-MIN_DIST = 100
+MIN_DIST = 20
 USING_CAMERA = True
+RESIZE_FRAME_WIDTH = 600
  
-def run_hough_circles(image):
+def run_hough_circles(image, earlier):
   output = image.copy()
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
   # kernel_size = 5
@@ -18,7 +20,7 @@ def run_hough_circles(image):
   # edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
 
   # detect circles in the image
-  circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, DP, MIN_DIST, param1=50, param2=30, minRadius=10, maxRadius=25)
+  circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, DP, MIN_DIST, param1=50, param2=25, minRadius=7, maxRadius=23)
    
   # ensure at least some circles were found
   if circles is not None:
@@ -37,15 +39,27 @@ def run_hough_circles(image):
     # show the output image
     cv2.imshow("output", output)
     cv2.waitKey(0)
+    now = datetime.now()
+    print(now - earlier)
   else:
     print("No circles :(")
+  return now
 
 def main():
   if USING_CAMERA:
-        cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
   
+  earlier = datetime.now()
   while True:
-
+    # frame = cv2.imread("test_imgs/1.jpg")
+    if USING_CAMERA:
+      ret, frame = cap.read()
+      frame_height = frame.shape[0]
+      frame_width = frame.shape[1]
+      resize_frame_height = int(frame_height / frame_width * RESIZE_FRAME_WIDTH)
+      frame = cv2.resize(frame, (RESIZE_FRAME_WIDTH, resize_frame_height))
+      now = run_hough_circles(frame, earlier)
+      earlier = now
   # # construct the argument parser and parse the arguments
   # ap = argparse.ArgumentParser()
   # ap.add_argument("-i", "--image", required = True, help = "Path to the image")

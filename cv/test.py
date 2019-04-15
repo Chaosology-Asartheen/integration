206598dabfdcf4dbef6e70e46783c1e02d833977
christinaou <christinaou@cmu.edu>
2019-04-15 11:55:34 -0400

    Remove unnecessary files and clean up test imgs

--- cv/test.py
+++ cv/test.py
@@ -1,77 +1,23 @@
+import ball_initializer
+import constants
 import cv2
 import numpy as np
 import imutils
 from datetime import datetime
 from houghlines import compute_lines
 
-# HSV values for different color balls
-# Range is 180,255,255
-lower_yellow = np.array([25,75,200]) #1
-upper_yellow = np.array([35,180,255])
-yellow_min_contour = .75
-yellow = (255,255,0)
-lower_orange = np.array([10,70,150]) #2
-upper_orange = np.array([24,180,255])
-orange_min_contour = .7
-orange = (255,140,0)
-lower_blue = np.array([98,200,80]) #3
-upper_blue = np.array([110,255,210])
-blue_min_contour = 1.2
-blue = (0,0,255)
-lower_purple = np.array([107,125,20]) #4
-upper_purple = np.array([130,190,140])
-purple_min_contour = .7
-purple = (138,43,226)
-lower_red = np.array([177,0,80]) #5
-upper_red = np.array([180,255,255])
-red_min_contour = .5
-red = (255,0,0)
-lower_green = np.array([89,204,0]) #6
-upper_green = np.array([92,230,255])
-green_min_contour = .6
-green = (0,128,0)
-lower_brown = np.array([170,100,0]) #7, hard
-upper_brown = np.array([179,180,180])
-brown_min_contour = .38
-brown = (165,42,42)
-lower_white = np.array([230,245,245]) #cue ball, using rgb as bounds
-upper_white = np.array([255,255,255])
-white_min_contour = .5
-white = (200,200,200)
-black = (0,0,0)
-lower_black = np.array([0,0,0])
-upper_black = np.array([70,70,70])
-black_min_contour = .7
-# upper_black = np.array([180,255,35])
+ball_list
 
-# cue_lower = np.array([14,10,200])
 # cue_upper = np.array([18,128,255])
 cue_lower = np.array([180,240,240]) #cue ball, using rgb as bounds
 cue_upper = np.array([255,255,255])
 
-BALL_RADIUS = .6
-TABLE_LENGTH = 37.5
-TABLE_WIDTH = 17.5625
-ESC_KEY = 27
+# Commandline arguments
 DISPLAY = False
 DISPLAY_INTERMEDIATE = False
 DISPLAY_HOUGH = True
-MAX_CUE_AREA = 1000
 USING_CAMERA = True
-MIN_RADIUS = .035
-# MAX_RADIUS = .05
-MAX_RADIUS = .065
-RESIZE_FRAME_WIDTH = 1400
-ESC_KEY = 27
 
-# Class to store information on each ball
-class BallInfo:
-    def __init__(self, lower_bound, upper_bound, rgb, str_rep, min_contour):
-        self.lower_bound = lower_bound
-        self.upper_bound = upper_bound
-        self.bgr = (rgb[2], rgb[1], rgb[0])
-        self.str_rep = str_rep
-        self.min_contour = min_contour
 
 class CVBall:
     def __init__(self, x, y, color):
@@ -85,29 +31,11 @@ class CVBall:
 def wait_escape():
     while(1):
         k = cv2.waitKey(5) & 0xFF
-        if k == ESC_KEY:
+        if k == constants.ESC_KEY:
             break
 
-def init_ballinfo():
-    balls = []
-    white_ball = BallInfo(lower_white, upper_white, white, 'white', white_min_contour)
-    balls.append(white_ball)
-    # yellow_ball = BallInfo(lower_yellow, upper_yellow, yellow, 'yellow', yellow_min_contour)
-    # balls.append(yellow_ball) # issues w 9 ball
-    # orange_ball = BallInfo(lower_orange, upper_orange, orange, 'orange', orange_min_contour)
-    # balls.append(orange_ball # issues
-    blue_ball = BallInfo(lower_blue, upper_blue, blue, 'blue', blue_min_contour)
-    balls.append(blue_ball)
-    purple_ball = BallInfo(lower_purple, upper_purple, purple, 'purple', purple_min_contour)
-    balls.append(purple_ball)
-    # red_ball = BallInfo(lower_red, upper_red, red, 'red', red_min_contour)
-    # balls.append(red_ball) # looks at brown
-    green_ball = BallInfo(lower_green, upper_green, green, 'green', green_min_contour)
-    balls.append(green_ball)
-    brown_ball = BallInfo(lower_brown, upper_brown, brown, 'brown', brown_min_contour)
-    balls.append(brown_ball)
-    # black_ball = BallInfo(lower_black, upper_black, black, 'black', black_min_contour)
-    # balls.append(black_ball) # issues
+def init_ball_info(ball_list):
+    ball_info = ball_initializer.init_balls(ball_list)
     return balls
 
 def norm_coordinates(x, y, min_x, max_x, min_y, max_y):
@@ -163,17 +91,17 @@ def find_ball(ball, hsv, frame, cv_balls):
 
         for c in circles:
             ((x, y), radius) = cv2.minEnclosingCircle(c)
-            # print("min: " + str(MIN_RADIUS * table_pixel_width) + " max: " + str(MAX_RADIUS * table_pixel_width) + " actual: " + str(radius))
+            # print("min: " + str(constants.MIN_RADIUS * table_pixel_width) + " max: " + str(constants.MAX_RADIUS * table_pixel_width) + " actual: " + str(radius))
             # only proceed if the radius meets a minimum size
-            # if (radius > MIN_RADIUS * table_pixel_width and 
-            #     radius < MAX_RADIUS * table_pixel_width):
+            # if (radius > constants.MIN_RADIUS * table_pixel_width and 
+            #     radius < constants.MAX_RADIUS * table_pixel_width):
             (norm_x, norm_y) = norm_coordinates(x, y, 0, table_pixel_length, 0, table_pixel_width)
-            table_x = norm_x * TABLE_LENGTH
-            table_y = norm_y * TABLE_WIDTH
+            table_x = norm_x * constants.TABLE_LENGTH
+            table_y = norm_y * constants.TABLE_WIDTH
             
             
-            if (BALL_RADIUS < table_x < TABLE_LENGTH-BALL_RADIUS and
-                BALL_RADIUS < table_y < TABLE_WIDTH-BALL_RADIUS):
+            if (constants.BALL_RADIUS < table_x < constants.TABLE_LENGTH-constants.BALL_RADIUS and
+                constants.BALL_RADIUS < table_y < constants.TABLE_WIDTH-constants.BALL_RADIUS):
                 # print("contour area: " + str(cv2.contourArea(c)) + " min: " + str(min_contour_area) + " max: " + str(max_contour_area))
                 if (min_contour_area < cv2.contourArea(c) < max_contour_area):  
                     # print("tpw %d tpl %d" % (table_pixel_width, table_pixel_length))
@@ -181,9 +109,9 @@ def find_ball(ball, hsv, frame, cv_balls):
                     # print("norm_x: %f norm_y: %f" % (norm_x, norm_y))
                     
                     print("table_x: %f table_y: %f" % (table_x, table_y))
-                    # print("mintx: %f minty: %f" % (2.5*BALL_RADIUS, TABLE_LENGTH-2.5*BALL_RADIUS))
-                    if (ball.str_rep != "white" or (2*BALL_RADIUS < table_x < TABLE_LENGTH-2*BALL_RADIUS and
-                        2*BALL_RADIUS < table_y < TABLE_WIDTH-2*BALL_RADIUS)):
+                    # print("mintx: %f minty: %f" % (2.5*constants.BALL_RADIUS, constants.TABLE_LENGTH-2.5*constants.BALL_RADIUS))
+                    if (ball.str_rep != "white" or (2*constants.BALL_RADIUS < table_x < constants.TABLE_LENGTH-2*constants.BALL_RADIUS and
+                        2*constants.BALL_RADIUS < table_y < constants.TABLE_WIDTH-2*constants.BALL_RADIUS)):
                         # print("within contour_area")  
                         
                         cv_balls.append(CVBall(norm_x, norm_y, ball.str_rep))
@@ -217,7 +145,7 @@ def find_cuestick(hsv, frame):
     table_pixel_length = frame.shape[1]
     table_pixel_width = frame.shape[0]
     mask = cv2.inRange(frame, cue_lower, cue_upper)
-    # mask = cv2.inRange(frame, lower_white, upper_white)
+    # mask = cv2.inRange(frame, LOWER_WHITE, UPPER_WHITE)
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask=mask)
 
@@ -266,8 +194,8 @@ def getResizedFrame(cap):
         frame = cv2.imread(filename)
     frame_height = frame.shape[0]
     frame_width = frame.shape[1]
-    resize_frame_height = int(frame_height / frame_width * RESIZE_FRAME_WIDTH)
-    frame = cv2.resize(frame, (RESIZE_FRAME_WIDTH, resize_frame_height))
+    resize_frame_height = int(frame_height / frame_width * constants.RESIZE_FRAME_WIDTH)
+    frame = cv2.resize(frame, (constants.RESIZE_FRAME_WIDTH, resize_frame_height))
 
     return frame
 
@@ -295,8 +223,9 @@ def show_edges():
         wait_escape()
 
 def main():
-    show_edges()
-    balls = init_ballinfo()
+    # show_edges()
+    ball_list = 
+    balls = init_ball_info(ball_list)
 
     if USING_CAMERA:
         cap = cv2.VideoCapture(0)
@@ -315,8 +244,8 @@ def main():
 
         # frame_height = frame.shape[0]
         # frame_width = frame.shape[1]
-        # resize_frame_height = int(frame_height / frame_width * RESIZE_FRAME_WIDTH)
-        # frame = cv2.resize(frame, (RESIZE_FRAME_WIDTH, resize_frame_height))
+        # resize_frame_height = int(frame_height / frame_width * constants.RESIZE_FRAME_WIDTH)
+        # frame = cv2.resize(frame, (constants.RESIZE_FRAME_WIDTH, resize_frame_height))
         frame = getResizedFrame(cap)
         # cv2.imwrite()
 
@@ -334,7 +263,6 @@ def main():
         x1,y1,x2,y2 = table_coords
         frame = frame[int(y1):int(y2),int(x1):int(x2)]
         
-        # cv2.imwrite('purple.jpg', frame)
         # Convert BGR to HSV
         hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         cv_balls = find_balls(balls, hsv_img, frame)
